2c7644cae2a51fb18b490e55911910e233ff44db
Nurkhotijah <nurkhotijahbogor@gmail.com>
2025-04-16 14:41:03 +0700

    push fe search admin

--- resources/views/dashboard-admin/data-peserta.blade.php
+++ resources/views/dashboard-admin/data-peserta.blade.php
@@ -7,19 +7,14 @@
             <!-- Search Bar -->
             <form action="{{ route('datapeserta-admin') }}" method="GET" class="w-full max-w-xs">
                 <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Cari</label>
-                <div class="relative flex items-center">
-                    <!-- Input Search -->
-                    <input type="search" name="search" id="search" 
-                        class="block w-full pl-4 pr-14 py-2.5 text-sm text-gray-700 border-2 border-sky-300 focus:outline-none rounded-full bg-gray-50" 
-                        placeholder="Cari Peserta (Nama, Email)" value="{{ request('search') }}" />
-                    <!-- Button Search -->
-                    <button type="submit" 
-                        class="absolute right-1 py-2.5 bg-sky-300 text-white hover:bg-sky-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-sm px-3 flex items-center justify-center">
-                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <div class="flex items-center border border-gray-300 rounded-lg bg-white">
+                        <svg class="w-4 h-4 text-gray-500 ml-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
-                    </button>
-                </div>
+                        <input type="search" name="search" id="search" 
+                            class="block w-full p-2 pl-2 text-sm text-gray-700 border-0 rounded-lg focus:border-sky-400 focus:outline-none" 
+                            placeholder="Cari Nama dan Email Peserta" value="{{ request('search') }}" />
+                    </div>
             </form>
 
             <div class="flex text-center justify-between">
--- resources/views/dashboard-admin/discount.blade.php
+++ resources/views/dashboard-admin/discount.blade.php
@@ -8,17 +8,14 @@
             <!-- searchbar -->
             <form action="{{ route('discount') }}" method="GET" class="w-full md:max-w-xs">
                 <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Cari</label>
-                <div class="relative flex items-center">
-                    <input type="search" name="search" id="search" 
-                        class="block w-full pl-4 pr-14 py-2.5 text-sm text-gray-700 border-2 border-sky-300 rounded-full focus:outline-none bg-gray-50" 
-                        placeholder="Cari Kode Kupon..." value="{{ request('search') }}" />
-                    <button type="submit" 
-                        class="absolute right-1 py-2.5 bg-sky-300 text-white hover:bg-sky-200 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-sm px-3 font-semibold flex items-center justify-center">
-                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <div class="flex items-center border border-gray-300 rounded-lg bg-white">
+                        <svg class="w-4 h-4 text-gray-500 ml-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
-                    </button>
-                </div>
+                        <input type="search" name="search" id="search" 
+                            class="block w-full p-2 pl-2 text-sm text-gray-700 border-0 rounded-lg focus:border-sky-400 focus:outline-none" 
+                            placeholder="Cari Kode Kupon..." value="{{ request('search') }}" />
+                    </div>
             </form>
             
             <!-- button tambah -->
